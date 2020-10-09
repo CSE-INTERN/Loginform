16ff41018dc98daa8e76c02528df484d1d44fd7e
mima <mima@cseinfo.co.jp>
2020-10-09 18:33:31 +0900

    KEY VALUE

--- InternKadai_LoginForm_Normal/InternKadai_LoginForm_Normal/LoginButton.Designer.cs
+++ InternKadai_LoginForm_Normal/InternKadai_LoginForm_Normal/LoginButton.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace InternKadai_LoginForm_Normal
 {
-    partial class pass
+    partial class LoginButton
     {
         /// <summary> 
         /// 必要なデザイナー変数です。
@@ -28,33 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox_Password = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // textBox_Password
+            // LoginButton
             // 
-            this.textBox_Password.Font = new System.Drawing.Font("MS UI Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.textBox_Password.Location = new System.Drawing.Point(0, 0);
-            this.textBox_Password.Margin = new System.Windows.Forms.Padding(0);
-            this.textBox_Password.Name = "textBox_Password";
-            this.textBox_Password.PasswordChar = '●';
-            this.textBox_Password.Size = new System.Drawing.Size(128, 21);
-            this.textBox_Password.TabIndex = 3;
-            // 
-            // pass
-            // 
-            this.Controls.Add(this.textBox_Password);
-            this.Margin = new System.Windows.Forms.Padding(0);
-            this.Name = "pass";
-            this.Size = new System.Drawing.Size(128, 21);
-            this.Leave += new System.EventHandler(this.pass_Leave);
+
+            this.Name = "LoginButton";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.TextBox textBox_Password;
     }
 }
