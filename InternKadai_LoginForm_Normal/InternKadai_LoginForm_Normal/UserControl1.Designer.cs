ed44a41e1f5c6966acc99061c6f46af418c683c5
mima <mima@cseinfo.co.jp>
2020-10-09 14:06:06 +0900

    アサイン

---
+++ InternKadai_LoginForm_Normal/InternKadai_LoginForm_Normal/UserControl1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace InternKadai_LoginForm_Normal
+{
+    partial class UserControl1
+    {
+        /// <summary> 
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region コンポーネント デザイナーで生成されたコード
+
+        /// <summary> 
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を 
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.errarlabel = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // errarlabel
+            // 
+            this.errarlabel.AutoSize = true;
+            this.errarlabel.Location = new System.Drawing.Point(138, 82);
+            this.errarlabel.Name = "errarlabel";
+            this.errarlabel.Size = new System.Drawing.Size(0, 12);
+            this.errarlabel.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(0, 206);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(426, 40);
+            this.panel1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(314, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(96, 33);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // UserControl1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.errarlabel);
+            this.Name = "UserControl1";
+            this.Size = new System.Drawing.Size(426, 249);
+            this.Load += new System.EventHandler(this.UserControl1_Load);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label errarlabel;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+    }
+}
